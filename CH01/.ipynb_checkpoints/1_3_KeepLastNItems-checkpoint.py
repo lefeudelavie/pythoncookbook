8ab876734acc25855dc1ee4d6dcf0ba0ffc81e4b
lefeudelavie <lefeudelavie@163.com>
2019-06-02 23:48:00 +0800

    larn new stuff at ch01

--- CH01/.ipynb_checkpoints/1_3_KeepLastNItems-checkpoint.py
+++ CH01/.ipynb_checkpoints/1_3_KeepLastNItems-checkpoint.py
@@ -9,10 +9,10 @@ def search(lines, pattern, history=5):
 
 if __name__ == '__main__':
     with open('somefile.txt') as f:
-        for line,prevlines in search(f, 'python' , 5):
+        for line,prevlines in search(f, 'python',5):
             for pline in prevlines:
-                print(pline,end ='')
-            print(line,end='')
-            print('_' * 20)
+                print('$$$$',pline,end='***')
+            print('####',line,end = '&&&')
+            print('_'*20)
 
 
